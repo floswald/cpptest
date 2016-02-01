fd1cf3e736e9e232af0b50ae967adec2383475b9
Florian Oswald <florian.oswald@gmail.com>
2016-02-01 10:29:32 +0100

    added stuff

--- blitz/matrixPower/power.cpp
+++ blitz/matrixPower/power.cpp
@@ -6,14 +6,22 @@
 using namespace std;
 using namespace blitz;
 
+
+Array<double,3> ArrayPow(Array<double,3>G,int p){
+    if (p==2){
+	return pow2(G);
+}
+}
+
 int main(){
 
-	Array<int,3> data(shape(2,2,4),FortranArray<3>());
-	Array<int,3> data2(shape(2,2,4),FortranArray<3>());
-	data = 2;
+	Array<double,3> data(shape(2,2,4),FortranArray<3>());
+	Array<double,3> data2(shape(2,2,4),FortranArray<3>());
+	data = 2.1;
 	
 	cout << "data " << data << endl;
 	data2 = pow2(data);
+	data2 = ArrayPow(data,2);
 	cout << "data^2 " << data2 << endl;
 	data2 = pow5(data);
 	cout << "data^5 " << data2 << endl;
