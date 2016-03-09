edaaf118d4e9fb0cb4f5dfddf6cabc6c64915421
florian oswald <florian.oswald@gmail.com>
2016-03-09 14:30:58 +0100

    added C++11

--- vector/max_nan.cpp
+++ vector/max_nan.cpp
@@ -13,6 +13,35 @@
 		max  = *v_iter;
 	}
 
+
+	// C++11 version
+// 	void vector_max(std::vector<double> v, double &max, int &imax){
+//     std::vector<double>::iterator v_iter;
+//     v_iter = std::max_element(v.begin(),v.end(),
+//     [] (auto x, auto y)
+//     {
+//         return x < y ? true : isnan(x);
+//     });
+//     imax = std::distance(v.begin(), v_iter);
+//     max  = *v_iter;
+// }
+
+	void vector_isnan_max(std::vector<double> v, double &max, int &imax){
+		std::vector<double>::iterator v_iter;
+	    std::vector<double>::size_type p=0;
+	    imax = -1;
+	    // max = -std::numeric_limits<double>::max();
+	    max = std::numeric_limits<double>::lowest();
+
+		for (v_iter=v.begin();v_iter!=v.end();v_iter++){
+			std::cout << "current max = " << max << std::endl;
+			if ((!std::isnan(*v_iter)) && (*v_iter>max)){
+				imax = p;
+				max  = *v_iter;
+			}
+		}
+	}
+
 	int main(){
 
 		std::vector<double> v_vec;
@@ -20,30 +49,56 @@
 		int imax;
 		double val;
 
-		std::cout << "test 1. " << std::endl;
+		// std::cout << "test 1. " << std::endl;
 
-		v_vec.push_back( -33.0 );
-		v_vec.push_back( -124.0 );
-		v_vec.push_back( -31.0 );
-		v_vec.push_back( 18.4 );
+		// v_vec.push_back( -33.0 );
+		// v_vec.push_back( -124.0 );
+		// v_vec.push_back( -31.0 );
+		// v_vec.push_back( 18.4 );
 
-		vector_max(v_vec,val,imax);
-		std::cout << "max(v_vec) = " << val << std::endl;
-		std::cout << "indmax(v_vec) = " << imax << std::endl;
+		// vector_max(v_vec,val,imax);
+		// std::cout << "max(v_vec) = " << val << std::endl;
+		// std::cout << "indmax(v_vec) = " << imax << std::endl;
 
-		std::cout << "test 2. " << std::endl;
+		// vector_isnan_max(v_vec,val,imax);
+		// std::cout << "isnanmax(v_vec) = " << val << std::endl;
+		// std::cout << "indisnanmax(v_vec) = " << imax << std::endl;
+
+		// std::cout << "test 2. " << std::endl;
+
+		// v_vec.clear();
+		// v_vec.push_back( NAN );
+		// v_vec.push_back( NAN );
+		// v_vec.push_back( NAN );
+		// v_vec.push_back( -33.0 );
+		// v_vec.push_back( -124.0 );
+		// v_vec.push_back( -31.0 );
+		// v_vec.push_back( 31.0 );
+
+		// vector_max(v_vec,val,imax);
+		// std::cout << "max(v_vec) = " << val << std::endl;
+		// std::cout << "indmax(v_vec) = " << imax << std::endl;
+
+		// vector_isnan_max(v_vec,val,imax);
+		// std::cout << "isnanmax(v_vec) = " << val << std::endl;
+		// std::cout << "indisnanmax(v_vec) = " << imax << std::endl;
+
+		std::cout << "test 3. " << std::endl;
 
 		v_vec.clear();
+		v_vec.push_back( -47.5);
 		v_vec.push_back( NAN );
-		v_vec.push_back( NAN );
-		v_vec.push_back( NAN );
-		v_vec.push_back( -33.0 );
-		v_vec.push_back( -124.0 );
-		v_vec.push_back( -31.0 );
-		v_vec.push_back( 31.0 );
+		v_vec.push_back( -37 );
+		v_vec.push_back( -304.07 );
+		v_vec.push_back( -2026 );
+		v_vec.push_back( -53 );
 
-		vector_max(v_vec,val,imax);
-		std::cout << "max(v_vec) = " << val << std::endl;
-		std::cout << "indmax(v_vec) = " << imax << std::endl;
+		// vector_max(v_vec,val,imax);
+		// std::cout << "max(v_vec) = " << val << std::endl;
+		// std::cout << "indmax(v_vec) = " << imax << std::endl;
+
+		vector_isnan_max(v_vec,val,imax);
+		std::cout << "isnanmax(v_vec) = " << val << std::endl;
+		std::cout << "indisnanmax(v_vec) = " << imax << std::endl;
 
 	};
